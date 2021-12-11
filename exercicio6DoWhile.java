341d8603c25ab45c4f56171884c8c2a3a7bf3e9b
Karolina Araújo Soares <karolina.soares@SUMUP.LOCAL>
2021-12-10 21:59:34 -0300

    Incluindo exercicio 6, lacos de repeticao Java_dowhile

---
+++ exercicio6DoWhile.java
@@ -0,0 +1,34 @@
+package LacosRepeticao16;
+
+import java.util.Scanner;
+
+public class exercicio6DoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int num,somaMedia=0;
+		float media;
+		
+		System.out.println("\nDigite um valor: ");
+		num = leia.nextInt();
+		
+		do
+		{
+			if(num%3==0)
+			{
+				somaMedia += num;
+			}
+			System.out.println("\nDigite um valor: ");
+			num = leia.nextInt();
+		}
+		while (num != 0);
+		
+		media = (float)somaMedia;
+		System.out.println("\nA média dos números múltiplos de 3 é: "+ media);
+		
+	}
+
+}
