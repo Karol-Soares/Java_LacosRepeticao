111113fa2683f8379bc9f5c2a52920bac790e084
Karolina Araújo Soares <karolina.soares@SUMUP.LOCAL>
2021-12-10 21:46:16 -0300

    Incluindo exercicio 1, lacos de repeticao Java

---
+++ exercicio1For.java
@@ -0,0 +1,20 @@
+package LacosRepeticao16;
+
+public class exercicio1For {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num=1000;
+		
+		for (num=1000;num<=1999;num++)
+		{
+			if (num%11 == 5)
+			{
+				System.out.println("\n"+num);
+			}
+		}
+
+	}
+
+}
